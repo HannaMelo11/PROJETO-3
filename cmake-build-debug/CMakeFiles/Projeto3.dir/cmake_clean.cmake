5c3db329d1b7fdd3f768066061b0d99ed5b85ea2
Gabriel <gabigo.machado@gmail.com>
2024-04-26 02:09:46 -0300

    revisado
    revisado ate a parte de listar os contatos

---
+++ cmake-build-debug/CMakeFiles/Projeto3.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projeto3.dir/biblioteca.c.obj"
+  "CMakeFiles/Projeto3.dir/biblioteca.c.obj.d"
+  "CMakeFiles/Projeto3.dir/main.c.obj"
+  "CMakeFiles/Projeto3.dir/main.c.obj.d"
+  "Projeto3.exe"
+  "Projeto3.exe.manifest"
+  "Projeto3.pdb"
+  "libProjeto3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projeto3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
