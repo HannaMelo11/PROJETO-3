ee7167dc90e3751bd36fa0f980b4360aa9950672
MajorTube9 <149215636+MajorTube9@users.noreply.github.com>
2024-05-24 18:08:24 -0300

    alterar dados do cliente na main
    colocando o alterar dados do cliente na main

--- biblioteca.h
+++ biblioteca.h
@@ -11,6 +11,7 @@ struct Contato {
 void adicionar_contato(struct Contato lista_contatos[], int *total_contatos);
 void listar_contatos(struct Contato lista_contatos[], int total_contatos);
 void deletar_contato(struct Contato lista_contatos[], int *total_contatos, char telefone[]);
+void modificar_contato(struct Contato lista_contatos[], int total_contatos, char telefone[]);
 void salvar_agenda(struct Contato lista_contatos[], int total_contatos, char *nome_arquivo);
 void carregar_agenda(struct Contato lista_contatos[], int *total_contatos, char *nome_arquivo);
 
